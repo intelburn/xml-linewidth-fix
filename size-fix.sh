48fa92cb9116f14bd002ec368d360b2f95426247
intelburn <=nerdacs@gmail.com>
2018-03-03 16:46:23 -0500

    Added Fix for Font Size

---
+++ size-fix.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#This script is written by nerdacs
+#This script is designed to help eash the transition in suptitles when going from SD to HD.
+#This file may be modified to enhance its usefulnes.
+#It is designed to take all files ending in *.xml in the working directory and change the linewidth.
+for subtitle in *.xml
+do
+	sed -i '' -e '/<parameterid>size<\/parameterid>/{
+		N;
+		N;
+		N;
+		N;
+		s/<value>.*<\/value>/<value>72<\/value>/;}' "$subtitle"
+	echo "$subtitle has been fixed"
+done
+echo "All files have been fixed"
